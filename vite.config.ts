2f2fc1301652bf7e26f728c6b36891fa86e22cfe
Chung, Lian <18393696+hikariNTU@users.noreply.github.com>
2024-04-02 21:43:08 +0800

    remove dead code

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    plugins: [
-      react(),
-      {
-        name: "build-script",
-        buildStart() {
-          if (command === "build") {
-            console.log(process.env.VITE_PUBLIC_SHARE_KEY);
-          }
-        },
-      },
-    ],
+    plugins: [react()],
   };
 });
